759af494453dbe69f51cc092bd4d993e593d2777
CrypterEmerald <mpeter007@gmail.com>
2021-11-16 16:25:32 +0100

    Fixed filepaths

--- ddns_config_cloudflare.sh
+++ ddns_config_cloudflare.sh
@@ -11,10 +11,10 @@ wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/cloudflar
 wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/cloudflare/update_dns_cloudflare.py
 
 # Replace templates with actual data
-sed -i "s/read_token_placeholder/$readtoken/g" /var/www/html/data/cloudflare-update-record_config.yaml
-sed -i "s/edit_token_placeholder/$edittoken/g" /var/www/html/data/cloudflare-update-record_config.yaml
-sed -i "s/zone_placeholder/$topdomain/g" /var/www/html/data/cloudflare-update-record_config.yaml
-sed -i "s/record_placeholder/$subdomain.$topdomain/g" /var/www/html/data/cloudflare-update-record_config.yaml
+sed -i "s/read_token_placeholder/$readtoken/g" /etc/ddns/cloudflare-update-record_config.yaml
+sed -i "s/edit_token_placeholder/$edittoken/g" /etc/ddns/cloudflare-update-record_config.yaml
+sed -i "s/zone_placeholder/$topdomain/g" /etc/ddns/cloudflare-update-record_config.yaml
+sed -i "s/record_placeholder/$subdomain.$topdomain/g" /etc/ddns/cloudflare-update-record_config.yaml
 
 # Run the script every 30 minutes
 crontab -l > updatedcron
