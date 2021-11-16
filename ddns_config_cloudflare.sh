d8b9b90cd16c6d86a4cc32c732cb0e759de00242
CrypterEmerald <mpeter007@gmail.com>
2021-11-16 14:53:36 +0100

    Correction for Cloudflare tokens

--- ddns_config_cloudflare.sh
+++ ddns_config_cloudflare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 read -p 'Your top-level domain name - do not include subdomains (e.g. example.com): ' topdomain
 read -p 'Your A record name (e.g. grocy): ' subdomain
-read -p 'Cloudflare API token: ' token
+read -p 'Cloudflare API token with EDIT permission: ' edittoken
+read -p 'Cloudflare API token with READ permission: ' readtoken
 
 # Download updater script & blank config
 mkdir /etc/ddns
@@ -10,8 +11,8 @@ wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/cloudflar
 wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/cloudflare/update_dns_cloudflare.py
 
 # Replace templates with actual data
-sed -i "s/read_token_placeholder/$token/g" /var/www/html/data/cloudflare-update-record_config.yaml
-sed -i "s/edit_token_placeholder/$token/g" /var/www/html/data/cloudflare-update-record_config.yaml
+sed -i "s/read_token_placeholder/$readtoken/g" /var/www/html/data/cloudflare-update-record_config.yaml
+sed -i "s/edit_token_placeholder/$edittoken/g" /var/www/html/data/cloudflare-update-record_config.yaml
 sed -i "s/zone_placeholder/$topdomain/g" /var/www/html/data/cloudflare-update-record_config.yaml
 sed -i "s/record_placeholder/$subdomain.$topdomain/g" /var/www/html/data/cloudflare-update-record_config.yaml
 
